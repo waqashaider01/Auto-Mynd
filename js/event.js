1c60a9f3b33d3bcdf9f9d03f217ff55f81d61053
Sobia Ghulam Nabi <09799.exe@gmail.com>
2023-05-10 17:37:31 +0500

    slider changes

--- js/event.js
+++ js/event.js
@@ -22,66 +22,66 @@ _gaq.push(["_trackPageview"]);
 
 // Section2
 
-// ("use strict");
+("use strict");
 
-// function carousel() {
-//   let carouselSlider = document.querySelector(".carousel__slider");
-//   let list = document.querySelector(".carousel__list");
-//   let item = document.querySelectorAll(".carousel__item");
-//   let list2;
+function carousel() {
+  let carouselSlider = document.querySelector(".carousel__slider");
+  let list = document.querySelector(".carousel__list");
+  let item = document.querySelectorAll(".carousel__item");
+  let list2;
 
-//   const speed = 1;
+  const speed = 1;
 
-//   const width = list.offsetWidth;
-//   let x = 0;
-//   let x2 = width;
+  const width = list.offsetWidth;
+  let x = 0;
+  let x2 = width;
 
-//   function clone() {
-//     list2 = list.cloneNode(true);
-//     carouselSlider.appendChild(list2);
-//     list2.style.left = `${width}px`;
-//   }
+  function clone() {
+    list2 = list.cloneNode(true);
+    carouselSlider.appendChild(list2);
+    list2.style.left = `${width}px`;
+  }
 
-//   function moveFirst() {
-//     x -= speed;
+  function moveFirst() {
+    x -= speed;
 
-//     if (width >= Math.abs(x)) {
-//       list.style.left = `${x}px`;
-//     } else {
-//       x = width;
-//     }
-//   }
+    if (width >= Math.abs(x)) {
+      list.style.left = `${x}px`;
+    } else {
+      x = width;
+    }
+  }
 
-//   function moveSecond() {
-//     x2 -= speed;
+  function moveSecond() {
+    x2 -= speed;
 
-//     if (list2.offsetWidth >= Math.abs(x2)) {
-//       list2.style.left = `${x2}px`;
-//     } else {
-//       x2 = width;
-//     }
-//   }
+    if (list2.offsetWidth >= Math.abs(x2)) {
+      list2.style.left = `${x2}px`;
+    } else {
+      x2 = width;
+    }
+  }
 
-//   function hover() {
-//     clearInterval(a);
-//     clearInterval(b);
-//   }
+  function hover() {
+    clearInterval(a);
+    clearInterval(b);
+  }
 
-//   function unhover() {
-//     a = setInterval(moveFirst, 10);
-//     b = setInterval(moveSecond, 10);
-//   }
+  function unhover() {
+    a = setInterval(moveFirst, 10);
+    b = setInterval(moveSecond, 10);
+  }
 
-//   clone();
+  clone();
 
-//   let a = setInterval(moveFirst, 10);
-//   let b = setInterval(moveSecond, 10);
+  let a = setInterval(moveFirst, 10);
+  let b = setInterval(moveSecond, 10);
 
-//   carouselSlider.addEventListener("mouseenter", hover);
-//   carouselSlider.addEventListener("mouseleave", unhover);
-// }
+  carouselSlider.addEventListener("mouseenter", hover);
+  carouselSlider.addEventListener("mouseleave", unhover);
+}
 
-// carousel();
+carousel();
 
 document.addEventListener("mousemove", parallax);
 function parallax(e) {
